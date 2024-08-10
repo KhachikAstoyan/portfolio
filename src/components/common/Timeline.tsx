3722ae2b2171784fe38fcad5b5e57fe313451750
Khachik Astoyan <kastoyan@griddynamics.com>
2024-08-10 16:56:42 +0400

    experience section styling

--- src/components/common/Timeline.tsx
+++ src/components/common/Timeline.tsx
@@ -13,9 +13,13 @@ export const Timeline: React.FC<TimelineProps> = ({ items }) => {
   return (
     <>
       <ol className="relative border-s border-neutral-700 flex flex-col gap-12 dark:border-gray-700">
-        {items.map(({ date, title, detailedText, company }) => (
-          <li className="ms-6 group" key={title}>
-            <div className="transition-colors absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 bg-neutral-700 group-hover:bg-indigo-600"></div>
+        {items.map(({ date, title, detailedText, company }, index) => (
+          <li className="ms-6" key={title}>
+            <div
+              className={`transition-colors absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 ${
+                index === 0 ? "bg-indigo-600" : "bg-neutral-700"
+              }`}
+            ></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
               {date}
             </time>
